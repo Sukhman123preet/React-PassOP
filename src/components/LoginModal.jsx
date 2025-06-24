de5f6fb5d90b9b6416804f3f54479004f43a11fd
sukhmanpreet536 <preetbadhan987@gmail.com>
2025-06-24 14:00:49 +0530

    c

--- src/components/LoginModal.jsx
+++ src/components/LoginModal.jsx
@@ -13,7 +13,7 @@ const LoginModal = ({ onClose, onSwitchToSignup }) => {
     setError('');
 
     try {
-      const res = await fetch("http://localhost:5000/api/login", {
+      const res = await fetch('${import.meta.env.VITE_SERVER_URI}/api/login', {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData)
